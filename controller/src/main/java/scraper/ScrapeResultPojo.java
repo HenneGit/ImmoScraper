8932d0aa5c5c7fe9218a3e18de0c6565c0abd4c1
Kevin <k.hoffmann@oskberlin.com>
2021-01-16 03:39:39 +0100

    SearchQuery embedded and draft for CSV

--- controller/src/main/java/scraper/ScrapeResultPojo.java
+++ controller/src/main/java/scraper/ScrapeResultPojo.java
@@ -1,12 +1,22 @@
 package scraper;
 
+import com.opencsv.bean.CsvBindByName;
 import org.oszimt.fa83.api.Entity;
+import org.oszimt.fa83.pojo.ScrapeQuery;
 
 public class ScrapeResultPojo implements Entity {
 
-    String url;
-    String oid;
+    private String url;
 
+    @CsvBindByName(column = "pk")
+    private String oid;
+
+    /**
+     * use for bean creation only.
+     */
+    public ScrapeResultPojo() {
+        //necessary for CSVToBeanReader use builder
+    }
 
     public ScrapeResultPojo(String url, String oid) {
         this.url = url;
--- controller/src/main/java/scraper/Scraper.java
+++ controller/src/main/java/scraper/Scraper.java
@@ -9,6 +9,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.oszimt.fa83.pojo.ScrapeQuery;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Scraper {
@@ -37,9 +38,13 @@ public class Scraper {
 
         List<String> resultIdList =  new ArrayList<String>();
         List<ScrapeResultPojo> resultList =  new ArrayList<ScrapeResultPojo>();
+
         try {
-            String searchUrl = "https://www.immobilienscout24.de/Suche/de/" + searchQuery + "/wohnung-mieten?enteredFrom=one_step_search";
-            HtmlPage page = client.getPage("https://www.immobilienscout24.de/Suche/de/berlin/berlin/wohnung-mieten?price=-500.0&livingspace=50.0-&pricetype=rentpermonth&enteredFrom=one_step_search");
+
+            ScrapeResultsFileWriter fileWriter = ScrapeResultsFileWriter.getInstance();
+            Collection<ScrapeResultPojo> fetchedIds = fileWriter.findAll();
+
+           HtmlPage page = client.getPage(query.toUrl());
             System.out.println(page.getPage());
             List<HtmlElement> listCollection = page.getDocumentElement().getElementsByAttribute("ul", "id", "resultListItems");
             Iterable<HtmlElement> listElements = new ArrayList<HtmlElement>();
@@ -47,7 +52,7 @@ public class Scraper {
                 for (HtmlElement element : list.getHtmlElementDescendants()) {
                     if (element.hasAttribute("data-id")) {
                         String oid = element.getAttribute("data-id");
-                        if (!resultIdList.contains(oid)) {
+                        if ((!resultIdList.contains(oid) && fetchedIds == null) || (!(fetchedIds == null) && !fetchedIds.contains(oid))) {
                             resultIdList.add(oid);
                         }
                     }
@@ -59,6 +64,8 @@ public class Scraper {
                 resultList.add(result);
                 System.out.println(result.getUrl());
             }
+            fileWriter.write(resultList);
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -68,8 +75,8 @@ public class Scraper {
     }
     public static void main(String[] args){
         Scraper debug = new Scraper();
-        ScrapeQuery query = new ScrapeQuery();
-
+        ScrapeQuery query = new ScrapeQuery.ScrapeQueryBuilder().city("berlin/berlin").roomSize("2.0").priceTo(500.0).space(5.0).build();
+        System.out.println(query.toUrl());
         debug.scrape(query);
     }
 
